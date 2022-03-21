33da932da86146d0473cec3b92b8e4dbf0e344db
yashwanth355 <95912283+yashwanth355@users.noreply.github.com>
2022-03-21 17:50:28 +0530

    Create getCompanyNames.go

---
+++ backend/lambda/usermodule/getCompanyNames.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "ccl-psql-dev.cclxlbtddgmn.ap-south-1.rds.amazonaws.com"
+	port     = 5432
+	user     = "postgres"
+	password = "Ccl_RDS_DB#2022"
+	dbname   = "ccldevdb"
+)
+
+type companyNames struct {
+	Compname string `json:"compname"`
+}
+
+func getCompanyNames(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	headers := map[string]string{"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"}
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Println(err)
+		return events.APIGatewayProxyResponse{500, headers, nil, err.Error(), false}, nil
+	}
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+
+	if err != nil {
+		log.Println(err)
+		return events.APIGatewayProxyResponse{500, headers, nil, err.Error(), false}, nil
+	}
+
+	fmt.Println("Connected!")
+
+	rows, err := db.Query(`SELECT compname FROM dbo.company_master`)
+
+	var companyNamesList []companyNames
+	defer rows.Close()
+	for rows.Next() {
+		var companyName companyNames
+		err = rows.Scan(&companyName.Compname)
+		companyNamesList = append(companyNamesList, companyName)
+	}
+	res, _ := json.Marshal(companyNamesList)
+	return events.APIGatewayProxyResponse{200, headers, nil, string(res), false}, nil
+}
+
+func main() {
+	lambda.Start(getCompanyNames)
+}
